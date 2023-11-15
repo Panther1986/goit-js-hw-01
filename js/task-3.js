af3f75d6f558cee578d7612ee27b0b485622d9fb
Panther1986 <polzovatel77@gmail.com>
2023-11-15 21:51:55 +0200

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,9 @@
 function getElementWidth(content, padding, border) {
-    let boxSizing = parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-    let borderBox = boxSizing;
- 
+    let totalWidth = parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+    let elementWidth = totalWidth;
 
-    return borderBox;
-
-
-};
+    return elementWidth;
+}
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
